6d392c6b84d3be603a6d5f072595e4f1f05cb7a0
steven.webster <steven.webster@burbank.working-age.local>
2020-10-31 15:09:02 +0000

    updated linting errors

--- data/Promises/promisesTest.js
+++ data/Promises/promisesTest.js
@@ -1,47 +1,57 @@
+/* eslint-disable no-console */
 // Simple example of creating a Promise and then calling and using the then() to handle it.
 // with a promise the pending promise is returned immediately and then the actions will start
 // if condition or action is satisfied the resolve function is called, and the value passed is sent to the then() method.
 
-
 // EXAMPLE ONE ///////////
 const testAPromise = (x) => {
-    return new Promise(function(resolve, reject){
-        if(x===1){                        // here you can complete an action and if it works call the resolve method
-            resolve("The value was 1");
-        } else {
-            reject("the value was not 1");   // if the action fails we call the reject method.
-        }
-    });
-}
+  // eslint-disable-next-line func-names
+  return new Promise(function (resolve, reject) {
+    if (x === 1) {
+      // here you can complete an action and if it works call the resolve method
+      resolve('The value was 1');
+    } else {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject('the value was not 1'); // if the action fails we call the reject method.
+    }
+  });
+};
 
-testAPromise(1).then(value => {
+testAPromise(1)
+  .then((value) => {
+    // eslint-disable-next-line no-console
     console.log(value);
-}).catch(err => {
+  })
+  .catch((err) => {
+    // eslint-disable-next-line no-console
     console.log(err);
-})
+  });
 
-
-testAPromise(2).then(value => {
+testAPromise(2)
+  .then((value) => {
     console.log(value);
-}).catch(err => {
+  })
+  .catch((err) => {
     console.log(err);
-})
-
+  });
 
 // EXAMPLE TWO /////    using arrow function
 
 const promiseExampleTwo = (x) => {
-    return new Promise((res, rej) => {
-        if(x ===1){
-            res("the promise worked!");
-        } else {
-            rej("went wrong");
-        }
-    })
-}
+  return new Promise((res, rej) => {
+    if (x === 1) {
+      res('the promise worked!');
+    } else {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      rej('went wrong');
+    }
+  });
+};
 
-promiseExampleTwo(1).then(val => {
+promiseExampleTwo(1)
+  .then((val) => {
     console.log(val);
-}).catch(err => {
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
